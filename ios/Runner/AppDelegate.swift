fe90b6b8f155865057dabbf2e394875a9b5aead4
mahmoudKorany <123369343+mahmoudKorany@users.noreply.github.com>
2024-05-16 18:22:22 +0300

    edit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import flutter_local_notifications
+import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    GeneratedPluginRegistrant.register(with: registry)}
+
     GeneratedPluginRegistrant.register(with: self)
+
+      if #available(iOS 10.0, *) {
+         UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      }
+      override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+             Messaging.messaging().apnsToken = deviceToken
+             super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+           }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
